34ec5888fb860229c69cc349f8b2a62a5b5e327a
Zhuyb <919220608@qq.com>
2019-05-16 00:34:32 +0800

    webpack管理资源

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import _ from 'lodash';
+import './style.css';
+import Data from './data.xml'
+var Icon = require('./Icon.jpg');
 function component() {
     var element = document.createElement('div');
   
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack','&#xe91b;'], ' ');
+    element.classList.add('hello');
+    
+    // 将图像添加到现有DIV
+    var myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+    console.log(Data)
   
     return element;
   }

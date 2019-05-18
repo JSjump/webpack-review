3a5cfb81e4eecd45710b184a3a401c43a040f9cd
Zhuyb <919220608@qq.com>
2019-05-18 16:55:43 +0800

    生产环境构建

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-import _ from 'lodash';
-import printMe from './print';
+import {cube} from './math';
+if(process.env.NODE_ENV !== 'production'){
+    console.log('Looks like we are in development mode');
+}
 
 function component() {
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
-  
-
-    element.innerHTML = _.join(['Hello', 'webpack','&#xe91b;'], ' ');
-
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn);
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+       var element = document.createElement('pre');
+    
+       element.innerHTML = [
+         'Hello webpack!',
+         '5 cubed is equal to ' + cube(5)
+       ].join('\n\n');
+    
+        return element;
+      }
+    
+      document.body.appendChild(component());

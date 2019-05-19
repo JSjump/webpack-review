3a76b338b961aeb4380a2c9cd26189704e673999
Zhuyb <919220608@qq.com>
2019-05-19 22:54:27 +0800

    缓存文档初始化

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,17 @@
 const path = require('path');
-// const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-    // another: './src/another-module.js'
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      title: 'Code Splitting'
+    entry: './src/index.js',
+    plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        title: 'Caching'
     })
-    // new webpack.config.optimization.splitChunks({
-    //   name:'common' // 指定公共bundule的名称
-    // })
   ],
-//   optimization:{
-//     splitChunks:{
-//         cacheGroups:{
-//              commons:{
-//                  name:"commons",
-//                  chunks:"initial",
-//                  minChunks:2
-//              }
-//         }
-//     }
-//   },
   output: {
-    filename: '[name].bundle.js',
-    chunkFilename:'[name].bundle.js',
+    filename:'[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
   }
 };

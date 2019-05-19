3d2984535b0701d6a0e74decfa8f8d2cc3ae6b47
Zhuyb <919220608@qq.com>
2019-05-19 23:30:40 +0800

    缓存优化

--- src/index.js
+++ src/index.js
@@ -1,37 +1,15 @@
-
 import _ from 'lodash';
+// import Print from './print';
 
-  function component() {
-//    async function getComponent() {
-        // return import(/* webpackChunkName: "lodash" */ 'lodash').then(_ => {
-        //          var element = document.createElement('div');
-            
-        //          element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-            
-        //          return element;
-            
-        //        }).catch(error => 'An error occurred while loading the component');
-        var element = document.createElement('div');
-        // const _=await import(/*webpackChunkName:"lodash"*/'lodash');
-           var button = document.createElement('button');
-            var br = document.createElement('br');
-
-        button.innerHTML = 'Click me and look at the console!';
-        element.innerHTML = _.join(['Hello','webpack'],' ');
-        element.appendChild(br);
-       element.appendChild(button);
-
-   // Note that because a network request is involved, some indication
-   // of loading would need to be shown in a production-level site/app.
-            button.onclick = e => import(/* webpackChunkName: "print" */ './print').then(module => {
-                var print = module.default;
-
-                print();
-            });
-        return element;
+function component() {
+    var element = document.createElement('div');
+  
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hessssssllo', 'webpackssss'], ' ');
+    // element.onclick = Print.bind(null, 'Hello webpack!');
+  
+    return element;
   }
-
+  
   document.body.appendChild(component());
-// getComponent().then(component => {
-// document.body.appendChild(component);
-// })
+  index.html
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,36 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const webpack = require('webpack');
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        main:'./src/index.js',
+        vendor:[
+            'lodash'
+        ]},
     plugins: [
-      new CleanWebpackPlugin(['dist']),
+      new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         title: 'Caching'
-    })
+    }),
+    new webpack.HashedModuleIdsPlugin(),
   ],
+  optimization:{
+    runtimeChunk: {
+        name: 'manifest'
+      },
+    splitChunks:{
+        cacheGroups:{
+        vendor: {
+            name: 'vendor',
+            chunks: 'initial',
+            priority: -10,
+            reuseExistingChunk: false,
+            test: /node_modules\/(.*)\.js/  
+        }
+    }
+  }
+},
   output: {
     filename:'[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
